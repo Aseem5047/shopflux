8de59340ecb08ac4942d2fe36acf367f987f7041
Aseem5047 <aseemgupta2002@gmail.com>
2026-01-21 13:37:48 +0530

    updated changes

--- client/src/auth/auth.service.ts
+++ client/src/auth/auth.service.ts
@@ -2,7 +2,7 @@ import apiClient from "@/api/axios";
 import type { LoginData, RegisterData } from "./auth.types";
 
 export const login = (data: LoginData) =>
-	apiClient.post("/api/auth/login", data, { withCredentials: true });
+	apiClient.post("/auth/login", data, { withCredentials: true });
 
 export const register = (data: RegisterData) =>
-	apiClient.post("/api/auth/register", data, { withCredentials: true });
+	apiClient.post("/auth/register", data, { withCredentials: true });
